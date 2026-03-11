7de07c7f0b9a17dda5b7c8d5fe3710184d74dfce
qiucan4 <qiucan4@huawei.com>
2026-03-11 11:47:35 +0800

    docs: changed the Ascendc ops header license statement to the standard CANN-2.0 statement
    Co-authored-by: qiucan4<qiucan4@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !41 merge master into master
    
    docs: changed the Ascendc ops header license statement to the standard CANN-2.0 statement
    
    Created-by: qiucan4
    Commit-by: qiucan4
    Merged-by: cann-robot
    Description: # Pull Request 模板
    ----
    
    ## 描述
    docs: 
    1. changed the Ascendc ops header license statement to the standard CANN-2.0 statement.
    2. modified the algorithm description in doc "gaussian_splatting_precise_intersection_optimization.md"
    
    ## 类型
    - [ ] Bug 修复
    - [ ] 新功能
    - [ ] 重构（即不是新增功能，也不是修改bug的代码变动）
    - [ ] 构建过程或辅助工具的变动
    - [x] 文档内容更新
    
    ## 如何测试
    将仓库内Ascendc算子对应的license修改为标准的CANN2.0许可证。
    将gaussian_splatting_precise_intersection_optimization.md文件中对于求交集算子的算法描述更新为最新算法。
    
    ## Checklist:
    - [x] 我的代码遵循这个项目的代码风格
    - [x] 我已经自己测试过我的代码
    - [x] 我已经更新了相应的文档
    - [x] 我已经在标题中正确使用了类型标签（例如：`feat`, `fix`, `refactor`, `docs`, `test`）
    
    ## 其他信息
    在这里可以添加任何与这个 Pull Request 相关的其他说明。
    
    
    See merge request: cann/cann-recipes-spatial-intelligence!41

--- ops/ascendc/meta_gauss_render/csrc/FlashGaussianBuildMask.cpp
+++ ops/ascendc/meta_gauss_render/csrc/FlashGaussianBuildMask.cpp
@@ -1,32 +1,10 @@
 /**
- * Adapted from
- * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
- * Copyright (c) 2019, Facebook CORPORATION.
- 
- * All rights reserved.
- * Licensed under the BSD 3-Clause License  (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * https://opensource.org/licenses/BSD-3-Clause
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
  */
 #include <string>
 #include <cmath>
